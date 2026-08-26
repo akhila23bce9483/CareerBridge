a437606fb3ee8c72253055c1ac46080bf35111d3
Kunja Akhila <akhila.23bce9483@vitapstudent.ac.in>
2026-08-26 15:44:23 +0530

    Add Spring Boot application

---
+++ src/main/java/com/careerbridge/CareerBridgeApplication.java
@@ -0,0 +1,5 @@
+package com.careerbridge;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CareerBridgeApplication { public static void main(String[] args){SpringApplication.run(CareerBridgeApplication.class,args);} }
